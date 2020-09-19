8aae29f104fa2c6d490e3906a5f2da82a3095474
Vanguard1138 <71548588+Vanguard1138@users.noreply.github.com>
2020-09-19 11:48:51 +0100

    Create currys.ts

---
+++ src/store/currys.ts
@@ -0,0 +1,20 @@
+import {Store} from './store';
+
+export const currys: Store = {
+	cartUrl: '',
+	links: [
+		{
+			brand: 'MSI',
+			model: 'gaming x trio',
+			url: 'https://www.currys.co.uk/gbuk/computing-accessories/components-upgrades/graphics-cards/msi-geforce-rtx-3080-10-gb-gaming-x-trio-graphics-card-10214425-pdt.html',
+			oosLabels: ['Not available to collect in or near', 'Not available for delivery ']
+		},
+		{
+			brand: 'Gigabyte',
+			model: 'gaming oc',
+			url: 'https://www.currys.co.uk/gbuk/computing-accessories/components-upgrades/graphics-cards/gigabyte-geforce-rtx-3080-10-gb-gaming-oc-graphics-card-10214434-pdt.html',
+			oosLabels: ['Not available to collect in or near', 'Not available for delivery ']
+		}
+	],
+	name: 'currys'
+};
